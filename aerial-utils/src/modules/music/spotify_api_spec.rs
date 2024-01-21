360d35f1ccffbf10bf23d588a30df480ec0b871d
Ofek Kandel <ofek.kandel@gmail.com>
2024-01-21 16:54:26 +0200

    Added active device check

--- aerial-utils/src/modules/music/spotify_api_spec.rs
+++ aerial-utils/src/modules/music/spotify_api_spec.rs
@@ -3,6 +3,7 @@ use crate::utils::api_spec::NoResponse;
 use crate::utils::ApiRequest;
 use crate::utils::ApiRequestSpec;
 use reqwest::Method;
+use serde::Deserialize;
 
 pub struct Pause;
 impl_endpoint!(Pause, Method::PUT, "me/player/pause", NoResponse);
@@ -15,3 +16,20 @@ impl_endpoint!(GotoNextTrack, Method::POST, "me/player/next", NoResponse);
 
 pub struct GotoPrevTrack;
 impl_endpoint!(GotoPrevTrack, Method::POST, "me/player/previous", NoResponse);
+
+pub struct GetPlaybackState;
+impl_endpoint!(GetPlaybackState, Method::GET, "me/player", PlaybackState);
+
+#[derive(Deserialize)]
+pub struct PlaybackState {
+    pub device: SpotifyDevice,
+}
+
+#[derive(Deserialize)]
+pub struct SpotifyDevice {
+    pub id: Option<String>,
+    pub is_active: bool,
+    pub name: String,
+    #[serde(alias = "type")]
+    pub device_type: String,
+}
