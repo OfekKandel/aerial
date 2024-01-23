f5f57584ac52cb7024f9a84024ba2f7922919650
Ofek Kandel <ofek.kandel@gmail.com>
2024-01-23 20:51:21 +0200

    Added toggle

--- aerial-utils/src/modules/music/spotify_client.rs
+++ aerial-utils/src/modules/music/spotify_client.rs
@@ -1,7 +1,7 @@
 use super::spotify_api_handler::SpotifyApiHandler;
 use super::spotify_api_spec::{GetCurrentTrack, GetPlaybackState, GotoNextTrack, GotoPrevTrack, Pause, PlayingState, Resume};
 use super::{AuthError, InitialAuthError, MusicClient};
-use crate::utils::{api_handler::ApiHandler, api_spec::NoResponse, config::SpotifyConfig, http::ResponseError, Cache};
+use crate::utils::{api_handler::ApiHandler, config::SpotifyConfig, http::ResponseError, Cache};
 use thiserror::Error;
 
 pub struct SpotifyClient {
@@ -23,24 +23,35 @@ pub enum SpotifyError {
 impl MusicClient for SpotifyClient {
     type Error = SpotifyError;
 
-    fn pause(&self) -> Result<NoResponse, SpotifyError> {
+    fn toggle(&self) -> Result<(), Self::Error> {
+        match self.get_playing_state()? {
+            Some(PlayingState::Playing) => self.pause(),
+            _ => self.resume(),
+        }
+    }
+
+    fn pause(&self) -> Result<(), SpotifyError> {
         self.verify_playing_state(PlayingState::Playing)?;
-        self.api_handler.make_request(&Pause).map_err(SpotifyError::ApiRequestError)
+        self.api_handler.make_request(&Pause).map_err(SpotifyError::ApiRequestError)?;
+        Ok(())
     }
 
-    fn resume(&self) -> Result<NoResponse, Self::Error> {
+    fn resume(&self) -> Result<(), Self::Error> {
         self.verify_playing_state(PlayingState::Paused)?;
-        self.api_handler.make_request(&Resume).map_err(SpotifyError::ApiRequestError)
+        self.api_handler.make_request(&Resume).map_err(SpotifyError::ApiRequestError)?;
+        Ok(())
     }
 
-    fn goto_next_track(&self) -> Result<NoResponse, Self::Error> {
+    fn goto_next_track(&self) -> Result<(), Self::Error> {
         self.verify_active_device()?;
-        self.api_handler.make_request(&GotoNextTrack).map_err(SpotifyError::ApiRequestError)
+        self.api_handler.make_request(&GotoNextTrack).map_err(SpotifyError::ApiRequestError)?;
+        Ok(())
     }
 
-    fn goto_prev_track(&self) -> Result<NoResponse, Self::Error> {
+    fn goto_prev_track(&self) -> Result<(), Self::Error> {
         self.verify_active_device()?;
-        self.api_handler.make_request(&GotoPrevTrack).map_err(SpotifyError::ApiRequestError)
+        self.api_handler.make_request(&GotoPrevTrack).map_err(SpotifyError::ApiRequestError)?;
+        Ok(())
     }
 
     fn print_curr_track(&self) -> Result<(), Self::Error> {
