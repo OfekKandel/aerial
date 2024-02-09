4c3f08145483d8f0ed90e043193d875ff3cf1860
Ofek Kandel <ofek.kandel@gmail.com>
2024-02-09 22:51:02 +0200

    Added support for track and context in spec-gen and ai

--- aerial-ai/main.py
+++ aerial-ai/main.py
@@ -5,74 +5,89 @@ import subprocess
 from openai import OpenAI
 from openai.types.chat.chat_completion_message import ChatCompletionMessage
 
-MODEL = 'gpt-4'
+MODEL = "gpt-4"
 
 
 # NOTE: The config is in config_ai.toml because aerial-utils uses config.toml
 # TODO: Fix config.toml colision
 def get_config():
-    with open('config_ai.toml', 'rb') as file:
+    with open("config_ai.toml", "rb") as file:
         return tomllib.load(file)
 
+
 def get_tools_spec():
-    with open('tools-spec.json', 'rb') as file:
+    with open("tools-spec.json", "rb") as file:
         return json.load(file)
 
+
 def add_chat(prompt: str, messages: list[dict]):
-    messages.append({
-        "role": "user",
-        "content": prompt
-    })
+    messages.append({"role": "user", "content": prompt})
+
 
 def make_completion(client: OpenAI, messages: list[dict], tools: list[dict]):
     completion = client.chat.completions.create(
-        model=MODEL,
-        messages=messages,
-        tools=tools
+        model=MODEL, messages=messages, tools=tools
     )
     completion = completion.choices[0].message
     messages.append(completion)
     return completion
 
-def handle_completion(message: ChatCompletionMessage, client: OpenAI, messages: list[dict], tools: list[dict]) -> ChatCompletionMessage:
+
+def handle_completion(
+    message: ChatCompletionMessage,
+    client: OpenAI,
+    messages: list[dict],
+    tools: list[dict],
+) -> ChatCompletionMessage:
     if not message.tool_calls:
         return message
-    
+
     for tool_call in message.tool_calls:
         # TODO: Add logger for debugging
-        tool_response = handle_function(tool_call.function.name, json.loads(tool_call.function.arguments), tools)
-        messages.append({
-            "tool_call_id": tool_call.id,
-            "role": "tool",
-            "name": tool_call.function.name,
-            "content": tool_response
-        })
+        tool_response = handle_function(
+            tool_call.function.name, json.loads(tool_call.function.arguments), tools
+        )
+        messages.append(
+            {
+                "tool_call_id": tool_call.id,
+                "role": "tool",
+                "name": tool_call.function.name,
+                "content": tool_response,
+            }
+        )
     completion = make_completion(client, messages, tools)
     return handle_completion(completion, client, messages, tools)
 
+
 def parse_arguments(arguments: dict, tools: list[dict], func_name: str) -> list[str]:
-    tool = next((tool for tool in tools if tool['function']['name'] == func_name))
-    res = []
-    for name, data in tool['function']['parameters']['properties'].items():
-        res.insert(data['index'], arguments[name])
-    return res
+    tool = next((tool for tool in tools if tool["function"]["name"] == func_name))
+    pos_args = []
+    other_args = []
+    for name, data in tool["function"]["parameters"]["properties"].items():
+        if "index" in data:
+            pos_args.insert(data["index"], arguments[name])
+        elif name in arguments:
+            other_args.append(f"--{name}")
+            other_args.append(str(arguments[name]))
+    print(f"{func_name} {pos_args + other_args}")
+    return pos_args + other_args
+
 
 def handle_function(name: str, arguments: dict, tools: list[dict]):
-    commands = ['./aerial-utils'] + name.split('_') + parse_arguments(arguments, tools, name)
-    process =  subprocess.run(commands, capture_output=True, text=True, cwd='.')
+    commands = (
+        ["./aerial-utils"] + name.split("_") + parse_arguments(arguments, tools, name)
+    )
+    process = subprocess.run(commands, capture_output=True, text=True, cwd=".")
     # TODO: This is a weird approach because the aerial-utils error is in stderr and not stdout
     return process.stdout + process.stderr
 
 
 def main():
     config = get_config()
-    client = OpenAI(api_key=config['openai']['api_key'])
+    client = OpenAI(api_key=config["openai"]["api_key"])
     tools = get_tools_spec()
 
-    messages = [{
-        'role': 'system',
-        'content': config['prompt']
-    }]
+    messages = [{"role": "system", "content": config["prompt"]}]
 
     while True:
         prompt = input("<User> ")
@@ -81,5 +96,6 @@ def main():
         completion = handle_completion(completion, client, messages, tools)
         print(f"<AI> {completion.content}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
