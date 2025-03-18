909f985019e7b38813ec3dbc2d381ef45689f6b0
Ofek Kandel <ofek.kandel@gmail.com>
2025-03-18 22:00:25 +0200

    Refactor: Converted main error to be a struct (not dyn box std::error)

--- aerial-utils/src/main.rs
+++ aerial-utils/src/main.rs
@@ -2,8 +2,9 @@ mod modules;
 mod utils;
 
 use clap::{Parser, Subcommand};
-use modules::{music::MusicArgs, print_subcommand_specs, Module, Music};
-use utils::{cache::Cache, Config};
+use modules::{music::{MusicArgs, MusicError}, print_subcommand_specs, Module, Music};
+use thiserror::Error;
+use utils::{cache::{Cache, CacheError}, config::ConfigError, Config};
 
 const CONFIG_PATH: &str = "./config.toml";
 
@@ -22,12 +23,22 @@ enum Modules {
     CommandSpecs,
 }
 
-// TODO: Make this a normal error
-fn run_module(module: Modules) -> Result<(), Box<dyn std::error::Error>> {
-    let mut cache = Cache::from_file("cache.toml")?;
-    let config = Config::from_file(CONFIG_PATH)?;
+#[derive(Error, Debug)]
+pub enum AppError {
+    #[error("Cache error: {0}")]
+    CacheError(CacheError),
+    #[error("Config error: {0}")]
+    ConfigError(ConfigError),
+    #[error("Music module error: {0}")]
+    MusicError(MusicError),
+}
+
+
+fn run_module(module: Modules) -> Result<(), AppError> {
+    let mut cache = Cache::from_file("cache.toml").map_err(AppError::CacheError)?;
+    let config = Config::from_file(CONFIG_PATH).map_err(AppError::ConfigError)?;
     let res = match module {
-        Modules::Music(args) => Ok(Music::run(args, &config, &mut cache)?),
+        Modules::Music(args) => Music::run(args, &config, &mut cache).map_err(AppError::MusicError),
         Modules::CommandSpecs => {
             print_subcommand_specs();
             Ok(())
@@ -35,7 +46,7 @@ fn run_module(module: Modules) -> Result<(), Box<dyn std::error::Error>> {
     };
     // NOTE: Cache won't be changed if the operation failed, might be good because
     // running the same command twice shouldn't get a different result
-    cache.to_file("cache.toml")?;
+    cache.to_file("cache.toml").map_err(AppError::CacheError)?;
     return res;
 }
 
